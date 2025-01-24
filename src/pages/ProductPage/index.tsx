49fcd867de9ed3e1f020c95fc6990ef53ed2653f
NikolayFrontendDeveloper <nickolay91509031@gmail.com>
2025-01-24 17:15:48 +0100

    feat: added basic product page

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -1,9 +1,16 @@
 import s from './styles.module.scss'
 import ProductSwiper from '../../components/ProductSwiper'
 import { SwiperContext } from '../../context/SwiperContext'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 
 export default function ProductPage() {
+    const [count, setCount] = useState<number>(1);
+    const maxCount = 10;
+    const minCount = 1;
+
+    const increment = (): void => setCount((prevCount) => prevCount === maxCount ? maxCount : prevCount + 1);
+    const decrement = (): void => setCount((prevCount) => prevCount === minCount ? minCount : prevCount - 1);
+
     const context = useContext(SwiperContext);
 
     if (!context) {
@@ -25,6 +32,17 @@ export default function ProductPage() {
                 <div className={s.product_main_box}>
                     <ProductSwiper></ProductSwiper>
                     <div className={s.product_description_box}>
+                        <h1 className={s.product_title}>Car Floor Mats</h1>
+                        <div className={s.stars_box}>
+                            <img src="/star.svg" alt="star" />
+                            <img src="/star.svg" alt="star" />
+                            <img src="/star.svg" alt="star" />
+                            <img src="/star.svg" alt="star" />
+                            <img src="/half-star.svg" alt="star" />
+                        </div>
+                        <p className={s.product_rate}>Rated 4.8 by 53 customers</p>
+                        <p className={s.product_price}>₴1,520.00</p>
+                        <p>Color Name: {}</p>
                         <div className={s.color_dots}>
                             {colorDots.map(({ color, index }) => (
                                 <button
@@ -39,6 +57,13 @@ export default function ProductPage() {
                                 </button>
                             ))}
                         </div>
+                        <div className={s.product_counter}>
+                            <button className={s.counter_decrement} onClick={decrement}>-</button>  
+                            <h2>{count}</h2>
+                            <button className={s.counter_increment} onClick={increment}>+</button>
+                        </div>
+                        <button className={s.btn_buy_it_now}>BUY IT NOW</button>
+                        <button className={s.btn_add_to_cart}>ADD TO CART</button>
                     </div>
                 </div>
                 <div className={s.product_secondary_box}></div>
